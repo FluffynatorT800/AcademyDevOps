4a9bfa6d91070372b889ac5c387595a5d7d6770a
FluffynatorT800 <keineidee1@gmx.de>
2023-03-14 11:48:10 +0100

    complete crud operations

--- customerapi/src/main/java/de/telekom/customerapi/CustomerController.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerController.java
@@ -3,8 +3,12 @@ package de.telekom.customerapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -35,5 +39,28 @@ public class CustomerController {
         } catch (NoSuchElementException e) {
             return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
         }
-    } 
+    }
+    
+    @PostMapping("/id")
+    public void add(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Customer customer, @PathVariable int id) {
+        try {
+            Customer existsCustomer = customerService.getCustomer(id);
+            customer.setId(id);
+            customerService.saveCustomer(existsCustomer);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id)
+    {
+        customerService.deleteCustomer(id);
+    }
 }
