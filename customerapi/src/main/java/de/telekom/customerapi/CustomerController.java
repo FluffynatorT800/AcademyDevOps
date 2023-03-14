257efb5fe615c625a2e87cf8e1b9ba7e8e1500bd
FluffynatorT800 <keineidee1@gmx.de>
2023-03-14 10:25:17 +0100

    expanded crud operations

--- customerapi/src/main/java/de/telekom/customerapi/CustomerController.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerController.java
@@ -1,10 +1,15 @@
 package de.telekom.customerapi;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -18,5 +23,19 @@ public class CustomerController {
     @GetMapping("")
     public List<Customer> index() {
         return customerRepository.findAll();
+    }
+
+    @PostMapping("/plus")
+    public Customer createcustomer(
+        @RequestBody Customer customer
+    )
+    {
+        return customerRepository.save(customer);
+    }
+
+    @GetMapping("/customer/{id}")
+    public Optional<Customer> getNoteByID(@PathVariable(value = "id") int customerId)
+    {
+        return customerRepository.findById(customerId);
     }
 }
