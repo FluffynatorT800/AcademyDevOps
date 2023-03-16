6f198f829b4693e5f0d79083fe0b555fa5c9846a
FluffynatorT800 <keineidee1@gmx.de>
2023-03-16 15:15:27 +0100

    Modified to work whitout depecrated module

--- customerapi/src/main/java/de/telekom/customerapi/SecurityConfig.java
+++ customerapi/src/main/java/de/telekom/customerapi/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,17 +18,19 @@ public class SecurityConfig  {
     private Environment env;
     String passAdmin = env.getProperty("spring.security.user.password");
     @Bean
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public WebSecurityCustomizer webSecurityCustomizer(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin")
                                 .password(passAdmin).roles("USER");
+        return null;
     }
 
     @Bean
-    public void configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("User")
                 .and().formLogin().loginPage("login,jsp")
                 .failureUrl("/login.jsp?error=1").loginProcessingUrl("/login")
 				.permitAll().and().logout()
 				.logoutSuccessUrl("/index.html");
+        return null;
     }
 }
