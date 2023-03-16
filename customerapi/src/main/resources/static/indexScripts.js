3ac003112d97d9722d46cf26ad7692f6ed3689b4
FluffynatorT800 <keineidee1@gmx.de>
2023-03-16 14:32:58 +0100

    removed security for now

--- customerapi/src/main/resources/static/indexScripts.js
+++ customerapi/src/main/resources/static/indexScripts.js
@@ -1,7 +1,3 @@
-var csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
-var csrfHeader = $("meta[name='_csrf_header']").attr("content");
-var csrfToken = $("meta[name='_csrf']").attr("content");
-
 function myFunction() {
         
     function getData(url, cb) {
