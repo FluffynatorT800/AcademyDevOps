96d752630eb4fafbc461302e880da9d68a5fc8e4
FluffynatorT800 <keineidee1@gmx.de>
2023-03-17 10:27:09 +0100

    adding csrf token back in

--- customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class CustomerapiApplication {
@@ -17,7 +18,7 @@ public class CustomerapiApplication {
 	public class SpringBootSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         return http.build();
     }
 }
