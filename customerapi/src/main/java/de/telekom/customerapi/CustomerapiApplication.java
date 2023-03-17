58895924f1e4ad02b515e9c3aead2633bd160946
FluffynatorT800 <keineidee1@gmx.de>
2023-03-17 11:25:24 +0100

    enabled web security

--- customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -14,8 +15,9 @@ public class CustomerapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerapiApplication.class, args);
 	}
+	@EnableWebSecurity
 	@Configuration
-public class SpringBootSecurityConfiguration {
+	public class SpringBootSecurityConfiguration {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
