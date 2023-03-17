101c98782f8b5b457658b92f8af9110c3858605c
FluffynatorT800 <keineidee1@gmx.de>
2023-03-17 11:32:54 +0100

    breaking customercontroller on purpose

--- customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
@@ -2,12 +2,6 @@ package de.telekom.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class CustomerapiApplication {
@@ -15,13 +9,7 @@ public class CustomerapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerapiApplication.class, args);
 	}
-	@EnableWebSecurity
-	@Configuration
-	public class SpringBootSecurityConfiguration {
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        return http.build();
+
     }
-}
-}
+
+
