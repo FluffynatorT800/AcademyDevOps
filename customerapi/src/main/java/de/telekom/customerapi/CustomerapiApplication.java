293cc8a0923d90408f62fb09e2a993e5d28b1012
FluffynatorT800 <keineidee1@gmx.de>
2023-03-17 11:43:09 +0100

    with password but no csrf

--- customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
+++ customerapi/src/main/java/de/telekom/customerapi/CustomerapiApplication.java
@@ -2,6 +2,10 @@ package de.telekom.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class CustomerapiApplication {
@@ -9,7 +13,12 @@ public class CustomerapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerapiApplication.class, args);
 	}
-
+	@Configuration
+		public class SpringBootSecurityConfiguration {
+    	@Bean
+   		 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     		   http.csrf().disable();
+      		   return http.build();
     }
-
-
+}
+}
